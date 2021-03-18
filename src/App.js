a6aaba08d53693d4e49e523e76cd2cde5b082e8a
unknown <kaba100@yandex.ru>
2021-03-18 20:08:56 +0700

    create base

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './numbers'
+import React from 'react';
 
-const number = 34;
-const text = 'Keep on going';
-const item = true;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {{
-          color: 'blue'
-        }}>
-          Hello World!
-        </p>
-        <p>
-          { count * length }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const element = (<div>
+  Hello World! <a href=" ">Go to hell out of here</a>
+  </div>);
+export { element };
