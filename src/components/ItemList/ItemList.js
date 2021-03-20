b17bea9e76cd537d25af7dcb93c032fdbdb0aa50
unknown <kaba100@yandex.ru>
2021-03-21 00:26:17 +0700

    modify files in accordance with recomendations

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Item from '../Item/Item';
 
 const ItemList = ({ todoItem }) => (<ul>
-    <li>{ todoItem } </li>
-    <li>{ 'Talk to friend.' }</li>
-    <li>{ 'Make one more step closer to my goal.' }</li>
+    <li><Item todoItem = { todoItem } /></li>
+    <li><Item todoItem = { 'Talk to friend.' } /></li>
+    <li><Item todoItem = { 'Make one more step closer to my goal.' } /></li>
 </ul>);
 
 export default ItemList;
--- src/numbers.js
+++
@@ -1,3 +0,0 @@
-const count = 29;
-const length = 2017;
-export { count, length }
