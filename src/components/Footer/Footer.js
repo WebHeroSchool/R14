fbf2543c6fdc3bc6d20722fd1bf099aa250faea4
unknown <kaba100@yandex.ru>
2021-03-22 16:40:21 +0700

    add styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const Footer = ({ count }) => (
-    <div>Осталось выполнить дел: {count} </div>);
+    <div className="base__footer">Осталось выполнить дел: {count} </div>);
 
 export default Footer;
