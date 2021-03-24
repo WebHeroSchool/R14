8b95a7084171fd7983e3d86717d6266a3c89ac7e
unknown <kaba100@yandex.ru>
2021-03-24 15:48:24 +0700

    made refactoring of code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,28 +4,31 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Create a new application.',
-      isDone: false
-    },
-    {
-      value: 'Talk to my friend.',
-      isDone: true
-    },
-    {
-      value: 'Make one more step closer to my goal.',
-      isDone: true
-    }
-  ];
-
-return (
-  <div className = {styles.base}>
-    <h1 className = {styles.title}>Important cases: </h1>
-    <InputItem /> 
-    <ItemList items = {items}/>
-    <Footer count = {1} />
-  </div>);
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Create a new application.',
+        isDone: false
+      },
+      {
+        value: 'Talk to my friend.',
+        isDone: true
+      },
+      {
+        value: 'Make one more step closer to my goal.',
+        isDone: true
+      }
+    ];
+  
+  return (
+    <div className = {styles.base}>
+      <h1 className = {styles.title}>Important cases: </h1>
+      <InputItem /> 
+      <ItemList items = {items}/>
+      <Footer count = {1} />
+    </div>);
+  }
 }
+
 export default App;
